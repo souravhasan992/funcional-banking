1cac2111800d63dac5e80fe43e031a786b7c3ed0
souravhasan992 <souravhasan992@gmail.com>
2021-08-26 03:11:42 +0600

    balance update updated to common function

--- js/banking.js
+++ js/banking.js
@@ -1,15 +1,26 @@
 function getInputValue(input){
-    const amountInputField = document.getElementById(input);
-    const amountInput = parseFloat(amountInputField.value); 
+    const amountInputField= document.getElementById(input);
+    const amountInput= parseFloat(amountInputField.value); 
     amountInputField.value = '';
     return amountInput;
 }
 function updateTotalField (totalFieldId,input){
+    // debugger;
     const totalField = document.getElementById(totalFieldId);
-    const total = parseFloat(totalField.innerText);
-    totalField.innerText = total + input;
+    const previousTotal = parseFloat(totalField.innerText);
+    totalField.innerText = previousTotal + input;
 }
-
+function updateBalance(depositeInput, isAdd){
+    const balanceTotalField = document.getElementById('balance-total');
+    const balanceTotal = parseFloat(balanceTotalField.innerText);
+    if( isAdd == true){
+        balanceTotalField.innerText = balanceTotal + depositeInput;
+    }
+    else{
+        balanceTotalField.innerText = balanceTotal - depositeInput;
+    }
+    
+}
 
 
 document.getElementById('deposite-btn').addEventListener('click',function(){
@@ -21,10 +32,10 @@ document.getElementById('deposite-btn').addEventListener('click',function(){
     depositeTotalField.innerText = depositeTotal + depositeInput; */
     updateTotalField('deposite-total',depositeInput);
     
-    const balanceTotalField = document.getElementById('balance-total');
+    /* const balanceTotalField = document.getElementById('balance-total');
     const balanceTotal = parseFloat(balanceTotalField.innerText);
-    balanceTotalField.innerText = balanceTotal + depositeInput;
-
+    balanceTotalField.innerText = balanceTotal + depositeInput; */
+    updateBalance(depositeInput, true)
     
 })
 document.getElementById('withdraw-btn').addEventListener('click',function(){
@@ -37,9 +48,9 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
 
     updateTotalField('withdraw-total',withdrawInput);
 
-    const balanceTotalField = document.getElementById('balance-total');
+    /* const balanceTotalField = document.getElementById('balance-total');
     const balanceTotal = parseFloat(balanceTotalField.innerText);    
-    balanceTotalField.innerText = balanceTotal - withdrawInput;
+    balanceTotalField.innerText = balanceTotal - withdrawInput; */
 
-
+    updateBalance(withdrawInput , false);
 })
